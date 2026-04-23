1aae06190c9c14c537d815621680cf85c37681f8
neruu00 <dnwogus4260@naver.com>
2026-04-23 15:49:39 +0900

    chore(setup): 불필요 레거시 파일 정리 및 프로젝트 설정 업데이트

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import CustomCanvas from '../../components/canvas/CustomCanvas';
-
-export default function Page() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <CustomCanvas />
-    </div>
-  );
-}
