1c0b49f52618d8d0ee8e6d69057d69b17dc213b2
neruu00 <dnwogus4260@naver.com>
2026-04-30 09:48:27 +0900

    chore: reorder eslint plugin imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,9 +8,9 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 import { FlatCompat } from '@eslint/eslintrc';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 import prettierConfig from 'eslint-config-prettier';
 import importPlugin from 'eslint-plugin-import';
-import pluginQuery from '@tanstack/eslint-plugin-query';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
